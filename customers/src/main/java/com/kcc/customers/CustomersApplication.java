ae506705e1e7926612feb1558f904f50dd10e5b6
karthik-M-95 <karthikappu1995@gmail.com>
2022-12-11 11:43:52 +0530

    commit config update 3

--- customers/src/main/java/com/kcc/customers/CustomersApplication.java
+++ customers/src/main/java/com/kcc/customers/CustomersApplication.java
@@ -3,6 +3,7 @@ package com.kcc.customers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CustomersApplication {
 
